357050b515484fc9d6d3a4ebe2981a0c71775057
Sen Du <dusen@hex-tech.net>
2019-03-18 10:51:53 +0800

    add product sales sync code

--- core/process.py
+++ core/process.py
@@ -75,6 +75,11 @@ def write_mapping(name, data):
 
 # 业务相关操作：格式化数据
 def format_data(table, data):
+    """
+    pos_store_sales_detail:  门店时段
+    pos_store_sales: 门店销售
+    pos_product_sales: 单品销售
+    """
     # 数据库ID -> str id
     str_id = str(data['_id'])
     if table == 'pos_store_sales_detail':
@@ -116,10 +121,42 @@ def format_data(table, data):
         all_cup_sum_net_amount = data['all_cup_sum_net_amount']
         created = date_to_str(data['created']) 
         return (str_id, day, month, year, sales_date, store_id, people, sum_discount, cancel_order_discount, pre_sell_amount, avg_item_count, all_order_net_amount, hex_sum_amount, sum_master_discount_amount, avg_order_amount, hex_sum_dis_amount, sum_amount, order_count, cancel_order_count, status, sum_net_amount, all_order_discount, cancel_order_amount, es_amount, sum_item_count, all_order_amount, all_order_count, all_cup_sum_item_count, all_cup_sum_net_amount, created)
+    elif table == 'pos_product_sales':
+        sales_date = date_to_str(data['sales_date'], sub='day')
+        store_id = data['store_id']
+        product_id = data['item_id']
+        product_trans_discount = data['item_trans_discount']
+        product_avg_net_amount = data['item_avg_net_amount']
+        product_discount = data['item_discount']
+        product_net_amount_rate = data['item_net_amount_rate']
+        product_in_discount = data['item_in_discount']
+        product_quantity = data['item_quantity']
+        product_cancel_amount = data['item_cancel_amount']
+        product_amount = data['item_amount']
+        product_in_quantity = data['item_in_quantity']
+        product_hundred_times_rate = data['item_hundred_times_rate']
+        product_is_deal_master = 1 if data['item_is_deal_master'] else 0
+        product_hex_net_amount = data['item_hex_net_amount']
+        product_in_amount = data['item_in_amount']
+        product_cancel_discount = data['item_cancel_discount']
+        product_quantity_rate = data['item_quantity_rate']
+        product_hex_discount = data['item_hex_discount']
+        product_net_amount = data['item_net_amount']
+        product_cancel_quantity = data['item_cancel_quantity']
+        all_cup_sum_product_count = data['all_cup_sum_item_count']
+        all_cup_sum_net_amount = data['all_cup_sum_net_amount']
+        status = data['status']
+        created = date_to_str(data['created']) 
+        return (str_id, sales_date, store_id, product_id, product_trans_discount, product_avg_net_amount, product_discount, product_net_amount_rate, product_in_discount, product_quantity, product_cancel_amount, product_amount, product_in_quantity, product_hundred_times_rate, product_is_deal_master, product_hex_net_amount, product_in_amount, product_cancel_discount, product_quantity_rate, product_hex_discount, product_net_amount, product_cancel_quantity, all_cup_sum_product_count, all_cup_sum_net_amount, status, created)
 
 
 # 业务相关操作：获取sql语句
 def get_sql(table):
+    """
+    pos_store_sales_detail:  门店时段
+    pos_store_sales: 门店销售
+    pos_product_sales: 单品销售
+    """
     if table == 'pos_store_sales_detail':
         return '''
             INSERT INTO store_hours(id, sales_date, store_id, amount, count, sales_amount, sales_count) 
@@ -129,4 +166,9 @@ def get_sql(table):
         return '''
             INSERT INTO store_sales(id, day, month, year, sales_date, store_id, people, sum_discount, cancel_order_discount, pre_sell_amount, avg_item_count, all_order_net_amount, hex_sum_amount, sum_master_discount_amount, avg_order_amount, hex_sum_dis_amount, sum_amount, order_count, cancel_order_count, status, sum_net_amount, all_order_discount, cancel_order_amount, es_amount, sum_item_count, all_order_amount, all_order_count, all_cup_sum_item_count, all_cup_sum_net_amount, created)
             VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        '''
+    elif table == 'pos_product_sales':
+        return '''
+            INSERT INTO product_sales(id, sales_date, store_id, product_id, product_trans_discount, product_avg_net_amount, product_discount, product_net_amount_rate, product_in_discount, product_quantity, product_cancel_amount, product_amount, product_in_quantity, product_hundred_times_rate, product_is_deal_master, product_hex_net_amount, product_in_amount, product_cancel_discount, product_quantity_rate, product_hex_discount, product_net_amount, product_cancel_quantity, all_cup_sum_product_count, all_cup_sum_net_amount, status, created)
+            VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
         '''
